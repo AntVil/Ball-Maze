28b0b2071eef532045f8384119643d21d15c6118
Anton Böhler <antboehler@gmail.com>
2022-03-29 22:10:53 +0200

    update

--- src/scripts/mains.js
+++ src/scripts/mains.js
@@ -1,9 +1,26 @@
 let game;
 
 window.onload = function(){
+    orientationHandle();
+
     game = new Game();
 }
 
-window.onorientationchange  = function(){
-    document.getElementsByTagName("html")[0].style.transform = `rotate(${window.orientation + 90}deg)`;
+window.onorientationchange = orientationHandle;
+
+function orientationHandle(){
+    document.getElementsByTagName("html")[0].style.transform = `rotate(${window.orientation - 90}deg)`
 }
+
+window.addEventListener("deviceorientation", function(event) {
+    // alpha: rotation around z-axis
+    var rotateDegrees = event.alpha;
+    // gamma: left to right
+    var leftToRight = event.gamma;
+    // beta: front back motion
+    var frontToBack = event.beta;
+
+    console.log(rotateDegrees, leftToRight, frontToBack)
+}, true);
+
+
