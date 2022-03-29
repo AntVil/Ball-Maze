6aec51e91c95a76cca757eeead4814a4e9540cdb
Anton Böhler <antboehler@gmail.com>
2022-03-29 16:12:33 +0200

    update

--- src/scripts/mains.js
+++ src/scripts/mains.js
@@ -3,3 +3,7 @@ let game;
 window.onload = function(){
     game = new Game();
 }
+
+window.onorientationchange  = function(){
+    document.getElementsByTagName("html")[0].style.transform = `rotate(${window.orientation}deg)`;
+}
