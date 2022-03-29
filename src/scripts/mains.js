e80bf034882769a187731be61174d0e1a8c81233
Anton Böhler <antboehler@gmail.com>
2022-03-29 17:28:28 +0200

    update

--- src/scripts/mains.js
+++ src/scripts/mains.js
@@ -5,5 +5,5 @@ window.onload = function(){
 }
 
 window.onorientationchange  = function(){
-    document.getElementsByTagName("html")[0].style.transform = `rotate(${window.orientation}deg)`;
+    document.getElementsByTagName("html")[0].style.transform = `rotate(${window.orientation + 90}deg)`;
 }
