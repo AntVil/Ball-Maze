e804c9602631f63bbb8bd73afd3b181196fa0885
Anton Böhler <antboehler@gmail.com>
2022-03-29 22:38:16 +0200

    update

--- src/scripts/mains.js
+++ src/scripts/mains.js
@@ -1,7 +1,12 @@
 let game;
+let can;
+let c;
 
 window.onload = function(){
     orientationHandle();
+
+    can = document.getElementById("canvas");
+    c = can.getContext("2d");
 
     game = new Game();
 }
@@ -9,19 +14,26 @@ window.onload = function(){
 window.onorientationchange = orientationHandle;
 
 function orientationHandle(){
-    let orientation = -window.orientation;
-    document.getElementsByTagName("html")[0].style.transform = `rotate(${orientation - 90}deg)`;
+    document.getElementsByTagName("html")[0].style.transform = `rotate(${-window.orientation - 90}deg)`;
 }
 
 window.addEventListener("deviceorientation", function(event) {
-    // alpha: rotation around z-axis
-    var rotateDegrees = event.alpha;
-    // gamma: left to right
-    var leftToRight = event.gamma;
-    // beta: front back motion
-    var frontToBack = event.beta;
+    let xAngle = event.beta;
+    let yAngle = event.gamma;
+
+    c.strokeStyle = "#00FF00";
+    
+    c.beginPath();
+    c.moveTo(can.width/2, can.height/2);
+    c.lineTo(can.width/2 + 10*Math.cos(xAngle), can.height/2 + 10*Math.sin(xAngle));
+    c.stroke();
+
+    c.strokeStyle = "#FF0000";
+
+    c.beginPath();
+    c.moveTo(can.width/2, can.height/2);
+    c.lineTo(can.width/2 + 10*Math.cos(yAngle), can.height/2 + 10*Math.sin(yAngle));
+    c.stroke();
 
     console.log(rotateDegrees, leftToRight, frontToBack)
 }, true);
-
-
